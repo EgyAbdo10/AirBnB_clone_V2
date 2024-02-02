12336c5609db52f5ecb7da68089348ab13914daa
EgyAbdo10 <egyabdo490@gmail.com>
2024-02-02 21:56:11 +0200

    added or modified a file

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- create a new user and a new databses
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
