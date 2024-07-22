3aa4b67f26ff0b7a54540682d637d71baf4f66f2
EgyAbdo10 <egyabdo490@gmail.com>
2024-07-22 22:58:25 +0300

    added or modified a file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""this file is a fabfile togenerate tgz archives"""
+from fabric.api import *
+from datetime import datetime
+
+
+env.hosts = ["ubuntu@100.25.144.102", "ubuntu@34.207.58.33"]
+
+
+def do_pack():
+    """pack files into a tgz then get from remote servers"""
+    # /data/web_static/
+    # web_static_<year><month><day><hour><minute><second>.tgz
+    date_time = str(datetime.now()).split(" ")
+    date_now = date_time[0].split("-")
+    time_now = date_time[1].split(":")
+    year, month, day = date_now[0], date_now[1], date_now[2]
+    hour, min, sec = time_now[0], time_now[1], int(float(time_now[2]))
+    file_name = f"web_static_{year}{month}{day}{hour}{min}{sec}.tgz"
+    local("mkdir -p versions")
+    local(f"tar -czf ./versions/{file_name} ./web_static/")
