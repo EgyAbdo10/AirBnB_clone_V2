71902c0b7f1e6fd07ac66887427d6d1eab6d492e
EgyAbdo10 <egyabdo490@gmail.com>
2024-07-22 18:31:51 +0300

    added or modified a file

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+# install nginx and make dirs
+if ! dpkg -l | grep nginx; then
+    sudo apt-get -y update
+    sudo apt-get install nginx
+fi
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+
+if test -f /data/web_static/current; then
+    rm -r /data/web_static/current
+fi
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sed '/server_name _;/a\location /hbnb_static {\n   alias /data/web_static/current}' /etc/nginx/sites-available/default
