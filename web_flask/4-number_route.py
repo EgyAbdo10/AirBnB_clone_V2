73d80bc9295143cfbe998c2a47f54c12a13eea7e
EgyAbdo10 <egyabdo490@gmail.com>
2024-07-28 20:01:47 +0300

    added or modified a file

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""this module creates starts a Flask web application"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def open_web():
+    """open web and return Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def open_web_2():
+    """open web app on page hbnb and return HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def get_c_text(text):
+    """open web app on page c and print 'C <text>' """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def get_python_text(text="is_cool"):
+    """open web app on page c and print 'python <text>' """
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number", strict_slashes=False)
+@app.route("/number/<int:num>", strict_slashes=False)
+def get_num(num):
+    """open web app on page and prints num is a number """
+    return f"{num} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
