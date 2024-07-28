97e9b6023ee56bc217c24c9a76fda470b9c03b1b
EgyAbdo10 <egyabdo490@gmail.com>
2024-07-28 19:31:04 +0300

    added or modified a file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,9 +7,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def open_web():
+    """open web and return """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
