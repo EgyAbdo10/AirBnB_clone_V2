8a6f0c27aa59f92caee5cf60fb51a0c1381cacb5
EgyAbdo10 <egyabdo490@gmail.com>
2024-07-28 19:50:40 +0300

    added or modified a file

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""this module creates starts a Flask web application"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def open_web():
+    """open web and return Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def open_web_2():
+    """open web app on page hbnb and return HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def get_c_text(text):
+    """open web app on page c and print 'C <text>' """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def get_python_text(text="is cool"):
+    """open web app on page c and print 'C <text>' """
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
