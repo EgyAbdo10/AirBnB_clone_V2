026361a7634b2108bcd08a843e34a20a8e9802c2
EgyAbdo10 <egyabdo490@gmail.com>
2024-07-30 00:28:09 +0300

    added or modified a file

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,21 +12,22 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
+    """cloas connection after every request"""
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def get_states():
-    states_dict = storage.all("State")
-    sorted_states_dict = dict(sorted(states_dict.items(),
-                              key=lambda item: (item[1].name, item[0])))
-    
-    cities_dict = storage.all("City")
-    sorted_cities_dict = dict(sorted(cities_dict.items(),
-                              key=lambda item: (item[1].name, item[0])))
+    """ get all states objects"""
+    states = storage.all("State").values()
+    # sorted_states_dict = dict(sorted(states_dict.items(),
+    #   key=lambda item: (item[1].name, item[0])))
+
+    # cities = storage.all("City").values()
+    # sorted_cities_dict = dict(sorted(cities_dict.items(),
+    #   key=lambda item: (item[1].name, item[0])))
     return render_template("7-states_list.html",
-                           states=sorted_states_dict.values(),
-                           cities=sorted_cities_dict.values())
+                           states=states)
 
 
 if __name__ == "__main__":
